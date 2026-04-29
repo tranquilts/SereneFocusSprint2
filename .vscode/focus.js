748544f4c3eead5be32e4730db42cbd534ee7fcc
khadijahi <khadijahislam602@gmail.com>
2026-04-29 23:20:47 +0100

    update final prototype version

--- .vscode/focus.js
+++ .vscode/focus.js
@@ -1,17 +1,41 @@
-//retrieve the stored study time
-let studyTime = localStorage.getItem("studyTime");
+let studyTime = Number(localStorage.getItem("studyTime")) || 25;
 //convert minutes to seconds
 let totalSeconds = studyTime * 60;
 //set remaining seconds 
 let remainingSeconds = totalSeconds;
 let timer = null;
 //locate progress bar
-let progressBar =  document.querySelector("progress");
-//locate play button
-let playButton = document.querySelector("")
+let progressBar =  document.querySelector(".progress-bar");
+//locate start button
+let startButton = document.querySelector(".start-btn");
 //locate pause button
+let pauseButton = document.querySelector(".pause-btn");
 //locate reset button
-//when play is clicked, begin the timer countdown
+let resetButton = document.querySelector(".reset-btn");
+
+startButton.onclick = function() {
+    clearInterval(timer);
+
+    timer = setInterval(function () {
+        remainingSeconds = remainingSeconds - 1;
+        //track the time remaining and update the progress bar
+        let progress = ((totalSeconds - remainingSeconds) / totalSeconds) * 100;
+        progressBar.value = progress;
+
+        if (remainingSeconds <=0) {
+            clearInterval(timer);
+            window.location.href="completion.html";
+        }
+    }, 1000);
+};
+
 //when pause is clicked, freeze the timer countdown
+pauseButton.onclick = function() {
+    clearInterval(timer);
+}
 //when reset is clicked, reset the session and restart the timer afresh
-//when timer goes down to zero, direct user to completion page
+resetButton.onclick = function() {
+    clearInterval(timer);
+    remainingSeconds = totalSeconds;
+    progressBar.value = 0;
+}
--- .vscode/index.js
+++ .vscode/index.js
@@ -1,16 +1,16 @@
 //Start with 25 minutes
-
+alert("correct index file");
 let studyTime = 25;
-let plusButton = document.querySelector('.plus-btn');
-let minusButton = document.querySelector('.minus-btn');
-let timerView = document.querySelector('.timer-view');
-let startButton = document.querySelector('.start-btn');
+let plusButton = document.querySelector(".plus-btn");
+let minusButton = document.querySelector(".minus-btn");
+let timerView = document.querySelector(".timer-view");
+let startButton = document.querySelector(".start-btn");
 
-timerView.textContent = studyTime;
+timerView.textContent = studyTime + " mins";
 
 plusButton.onclick = function() {
     studyTime = studyTime + 5;
-    timerView.textContent = studyTime;
+    timerView.textContent = studyTime + " mins";
 };
 
 minusButton.onclick = function() {
